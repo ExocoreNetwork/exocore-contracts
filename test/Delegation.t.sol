c79461a36768623b0780de3c5a046f96b485ad41
adu <foriteration@gmail.com>
2023-11-07 11:55:53 +0800

    add delegation test

---
+++ test/Delegation.t.sol
@@ -0,0 +1,71 @@
+pragma solidity ^0.8.19;
+
+import "./ExocoreDeployer.t.sol";
+import "forge-std/Test.sol";
+import "../src/core/ExocoreReceiver.sol";
+import "../src/storage/GatewayStorage.sol";
+import "../src/interfaces/IGateway.sol";
+import "forge-std/console.sol";
+
+contract DepositWithdrawTest is ExocoreDeployer {
+    event InterchainMsgReceived(
+        uint16 indexed srcChainID,
+        bytes indexed srcChainAddress,
+        uint64 indexed nonce,
+        bytes payload
+    );
+    event SetTrustedRemote(uint16 _remoteChainId, bytes _path);
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+    event RequestSent(GatewayStorage.Action indexed act, uint16 indexed dstChainID, address indexed  dstAddress, bytes payload);
+    event MessageProcessed(uint16 _srcChainId, bytes _srcAddress, uint64 _nonce, bytes _payload);
+
+    function test_Delegation() public {
+        // -- deposit workflow test -- 
+
+        vm.chainId(clientChainID);
+
+        Player memory depositor = players[0];
+        vm.startPrank(exocoreValidatorSet.addr);
+        restakeToken.transfer(depositor.addr, 1000000);
+        vm.expectEmit(false, false, false, true);
+        emit SetTrustedRemote(exocoreChainID, abi.encodePacked(address(exocoreReceiver), address(gateway)));
+        gateway.setTrustedRemote(exocoreChainID, abi.encodePacked(address(exocoreReceiver), address(gateway)));
+        vm.expectEmit(false, false, false, true);
+        emit SetTrustedRemote(clientChainID, abi.encodePacked(address(gateway), address(exocoreReceiver)));
+        exocoreReceiver.setTrustedRemote(clientChainID, abi.encodePacked(address(gateway), address(exocoreReceiver)));
+        vm.stopPrank();
+
+        vm.startPrank(depositor.addr);
+        deal(address(gateway), 1e22);
+        restakeToken.approve(address(vault), type(uint256).max);
+        uint256 depositAmount = 10000;
+        bytes memory payload = abi.encodePacked(
+            GatewayStorage.Action.DEPOSIT, 
+            bytes32(bytes20(address(restakeToken))), 
+            bytes32(bytes20(depositor.addr)), 
+            depositAmount
+        );
+        vm.expectEmit(true, true, false, true);
+        emit Transfer(depositor.addr, address(vault), depositAmount);
+
+        // assert that exocoreReceiver should receive the message and save the msg as event
+        vm.expectEmit(true, true, true, true, address(exocoreReceiver));
+        emit InterchainMsgReceived(clientChainID, abi.encodePacked(bytes20(address(gateway))), 1, payload);
+        gateway.deposit(address(restakeToken), depositAmount);
+
+        // -- delegate workflow -- 
+
+        uint256 delegateAmount = 100;
+        Player memory operator = players[1];
+        payload = abi.encodePacked(
+            GatewayStorage.Action.DELEGATETO,
+            address(restakeToken),
+            bytes32(bytes20(operator.addr)),
+            bytes32(bytes20(depositor.addr)),
+            delegateAmount
+        );
+        vm.expectEmit(true, true, true, true, address(exocoreReceiver));
+        emit InterchainMsgReceived(clientChainID, abi.encodePacked(bytes20(address(gateway))), 2, payload);
+        gateway.delegateTo(bytes32(bytes20(operator.addr)), address(restakeToken), delegateAmount);
+    }
+}
