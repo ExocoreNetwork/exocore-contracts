6c09dc3796912167af45fee9d086040fca2df394
adu <foriteration@gmail.com>
2024-02-24 17:37:31 +0800

    implement capsule deposit

--- src/storage/ExoCapsuleStorage.sol
+++ src/storage/ExoCapsuleStorage.sol
@@ -1,7 +1,26 @@
 pragma solidity ^0.8.19;
 
 contract ExoCapsuleStorage {
+    enum VALIDATOR_STATUS {
+        INACTIVE, // doesnt exist
+        ACTIVE, // staked on ethpos and withdrawal credentials are pointed to the EigenPod
+        WITHDRAWN // withdrawn from the Beacon Chain
+    }
+
+    struct ValidatorInfo {
+        // index of the validator in the beacon chain
+        uint64 validatorIndex;
+        // amount of beacon chain ETH restaked on EigenLayer in gwei
+        uint64 restakedBalanceGwei;
+        //timestamp of the validator's most recent balance update
+        uint64 mostRecentBalanceUpdateTimestamp;
+        // status of the validator
+        VALIDATOR_STATUS status;
+    }
+
+    address public constant BEACON_ROOTS_ADDRESS = 0x000F3df6D732807Ef1319fB7B8bB8522d0Beac02;
     address payable exocoreValidatorSetAddress;
+    mapping(bytes32 pubkey => ValidatorInfo info) validatorStore;
 
     uint256[40] private __gap;
 }
