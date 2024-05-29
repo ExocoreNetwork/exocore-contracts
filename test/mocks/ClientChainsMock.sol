6fac2fa4441ee7b34980d29fcdc67c86aa4cace9
MaxMustermann2 <82761650+MaxMustermann2@users.noreply.github.com>
2024-05-29 17:35:17 +0000

    fix(mock): return testnet chain id

--- test/mocks/ClientChainsMock.sol
+++ test/mocks/ClientChainsMock.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.19;
 import {IClientChains} from "../../src/interfaces/precompiles/IClientChains.sol";
 
 contract ClientChainsMock is IClientChains {
-    uint16 clientChainId = 2;
+    uint16 clientChainId = 40161;
 
     function getClientChains() external view returns (bool, uint16[] memory) {
         uint16[] memory res = new uint16[](1);
