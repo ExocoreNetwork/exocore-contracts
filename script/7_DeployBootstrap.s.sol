bb70afa4dbcd008879f3f485ffd4fad6fc8abf92
MaxMustermann2 <82761650+MaxMustermann2@users.noreply.github.com>
2024-05-31 04:09:00 +0000

    fix(bootstrap-deploy): use prereq proxy bytecode

--- script/7_DeployBootstrap.s.sol
+++ script/7_DeployBootstrap.s.sol
@@ -15,6 +15,8 @@ import {BaseScript} from "./BaseScript.sol";
 import {ILayerZeroEndpointV2} from "@layerzero-v2/protocol/contracts/interfaces/ILayerZeroEndpointV2.sol";
 import {ERC20PresetFixedSupply} from "@openzeppelin-contracts/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
 
+import "@beacon-oracle/contracts/src/EigenLayerBeaconOracle.sol";
+
 contract DeployBootstrapOnly is BaseScript {
     function setUp() public virtual override {
         // load keys
@@ -24,10 +26,34 @@ contract DeployBootstrapOnly is BaseScript {
         clientChainLzEndpoint = ILayerZeroEndpointV2(
             stdJson.readAddress(prerequisiteContracts, ".clientChain.lzEndpoint")
         );
+        require(
+            address(clientChainLzEndpoint) != address(0),
+            "Client chain endpoint not found"
+        );
         restakeToken = ERC20PresetFixedSupply(
             stdJson.readAddress(prerequisiteContracts, ".clientChain.erc20Token")
         );
+        require(
+            address(restakeToken) != address(0),
+            "Restake token not found"
+        );
         clientChain = vm.createSelectFork(clientChainRPCURL);
+        // we should use the pre-requisite to save gas instead of deploying our own
+        beaconOracle = EigenLayerBeaconOracle(
+            stdJson.readAddress(prerequisiteContracts, ".clientChain.beaconOracle")
+        );
+        require(
+            address(beaconOracle) != address(0),
+            "Beacon oracle not found"
+        );
+        // same for BeaconProxyBytecode
+        beaconProxyBytecode = BeaconProxyBytecode(
+            stdJson.readAddress(prerequisiteContracts, ".clientChain.beaconProxyBytecode")
+        );
+        require(
+            address(beaconProxyBytecode) != address(0),
+            "Beacon proxy bytecode not found"
+        );
     }
 
     function run() public {
@@ -40,8 +66,6 @@ contract DeployBootstrapOnly is BaseScript {
         // vault, shared between bootstrap and client chain gateway
         vaultImplementation = new Vault();
         vaultBeacon = new UpgradeableBeacon(address(vaultImplementation));
-        // proxy bytecode, also shared between the two
-        beaconProxyBytecode = new BeaconProxyBytecode();
         // bootstrap logic
         Bootstrap bootstrapLogic = new Bootstrap(
             address(clientChainLzEndpoint),
@@ -57,7 +81,7 @@ contract DeployBootstrapOnly is BaseScript {
                     abi.encodeCall(Bootstrap.initialize,
                         (
                             exocoreValidatorSet.addr,
-                            block.timestamp + 365 days,
+                            block.timestamp + 365 days + 24 hours,
                             24 hours,
                             payable(exocoreValidatorSet.addr),
                             whitelistTokens, // vault is auto deployed
@@ -67,14 +91,11 @@ contract DeployBootstrapOnly is BaseScript {
                 ))
             )
         );
-        console.log("Bootstrap logic: ", address(bootstrapLogic));
-        console.log("Bootstrap address: ", address(bootstrap));
 
         // initialize proxyAdmin with bootstrap address
         proxyAdmin.initialize(address(bootstrap));
 
         // now, focus on the client chain constructor
-        beaconOracle = _deployBeaconOracle();
         capsuleImplementation = new ExoCapsule();
         capsuleBeacon = new UpgradeableBeacon(address(capsuleImplementation));
         ClientChainGateway clientGatewayLogic = new ClientChainGateway(
@@ -96,7 +117,6 @@ contract DeployBootstrapOnly is BaseScript {
             address(clientGatewayLogic),
             initialization
         );
-        console.log("Client chain gateway logic: ", address(clientGatewayLogic));
 
         vm.stopBroadcast();
 
@@ -122,3 +142,7 @@ contract DeployBootstrapOnly is BaseScript {
         vm.writeJson(finalJson, "script/deployedBootstrapOnly.json");
     }
 }
+
+// forge verify-contract --chain 11155111 --num-of-optimizations 200 --compiler-version "0.8.22" --constructor-args $(cast abi-encode "constructor(address,uint32,address,address,address,address)" "0x6EDCE65403992e310A62460808c4b910D972f10f" 40259 "0xd3D285cd1516038dAED61B8BF7Ae2daD63662492" "0x2899181D6EB55847165cfa3288E4708dA5070751" "0xe87e516C7116eC4DcFC5408c05618De6e1Cd4c10" "0xA15Ce26ba8E50ac21ecDa1791BAa3bf22a95b575") --watch --etherscan-api-key RDD5I2JQPJ8GSP4AMEFYJI9GYBWH1E2G95 0xcE10583b1Efe34319812d96c7edFFD71E8403ba2 ClientChainGateway
+
+// forge verify-contract --chain 11155111 --num-of-optimizations 200 --compiler-version "0.8.22" --watch --etherscan-api-key RDD5I2JQPJ8GSP4AMEFYJI9GYBWH1E2G95 --constructor-args $(cast abi-encode "constructor(address)" "0xF22097E6799DF7D8b25CCeF6E64DA3CB9133012D") 0x2899181D6EB55847165cfa3288E4708dA5070751 UpgradeableBeacon
