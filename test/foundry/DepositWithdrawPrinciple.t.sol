b30f4eb116994eb21af52796ed950d30b51d3d79
MaxMustermann2 <82761650+MaxMustermann2@users.noreply.github.com>
2024-06-21 04:39:44 +0000

    chore(fmt): run `forge fmt`

--- test/foundry/DepositWithdrawPrinciple.t.sol
+++ test/foundry/DepositWithdrawPrinciple.t.sol
@@ -2,8 +2,9 @@ pragma solidity ^0.8.19;
 
 import "../../src/core/ExoCapsule.sol";
 import "../../src/core/ExocoreGateway.sol";
-import {ILSTRestakingController} from "../../src/interfaces/ILSTRestakingController.sol";
+
 import {IExoCapsule} from "../../src/interfaces/IExoCapsule.sol";
+import {ILSTRestakingController} from "../../src/interfaces/ILSTRestakingController.sol";
 import "../../src/storage/GatewayStorage.sol";
 import "./ExocoreDeployer.t.sol";
 import "forge-std/Test.sol";
