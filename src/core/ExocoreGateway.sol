5c00ab095dc967ee94a6a35532de26c9bc56095b
adu <foriteration@gmail.com>
2024-08-01 11:33:07 +0800

    Update src/core/ExocoreGateway.sol

--- src/core/ExocoreGateway.sol
+++ src/core/ExocoreGateway.sol
@@ -170,7 +170,7 @@ contract ExocoreGateway is
     }
 
     /// @inheritdoc IExocoreGateway
-    /// @notice Tokens can only be normal reward-bearong LST tokens like wstETH, rETH, jitoSol...
+    /// @notice Tokens can only be normal reward-bearing LST tokens like wstETH, rETH, jitoSol...
     /// And they are not intended to be: 1) rebasing tokens like stETH, since we assume staker's
     /// balance would not change if nothing is done after deposit, 2) fee-on-transfer tokens, since we
     /// assume Vault would account for the amount that staker transfers to it.
