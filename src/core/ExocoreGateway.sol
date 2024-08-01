949e2e334ccd83559d881d302230991ba46abc59
adu <foriteration@gmail.com>
2024-08-01 10:51:19 +0800

    doc: notice native restaking virtual token address

--- src/core/ExocoreGateway.sol
+++ src/core/ExocoreGateway.sol
@@ -170,10 +170,13 @@ contract ExocoreGateway is
     }
 
     /// @inheritdoc IExocoreGateway
-    /// @dev tokens can only be normal reward-bearong LST tokens like wstETH, eETH, jitoSol...
+    /// @notice Tokens can only be normal reward-bearong LST tokens like wstETH, eETH, jitoSol...
     /// And they are not intended to be: 1) rebasing tokens like stETH, since we assume staker's
     /// balance would not change if nothing is done after deposit, 2) fee-on-transfer tokens, since we
     /// assume Vault would count for the amount that staker transfers to it.
+    /// @notice If we want to activate client chain's native restaking, we should add the corresponding virtual
+    /// token address to the whitelist, bytes32(bytes20(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE)) for Ethereum
+    /// native restaking for example.
     function addOrUpdateWhitelistTokens(
         uint32 clientChainId,
         bytes32[] calldata tokens,
