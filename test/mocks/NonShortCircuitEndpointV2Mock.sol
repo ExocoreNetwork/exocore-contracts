9fd6d6a320eaf1a9efce9206b8774ea9f1484bbd
adu <foriteration@gmail.com>
2024-08-22 12:51:13 +0800

    fix: forge fmt

--- test/mocks/NonShortCircuitEndpointV2Mock.sol
+++ test/mocks/NonShortCircuitEndpointV2Mock.sol
@@ -717,7 +717,8 @@ contract NonShortCircuitEndpointV2Mock is ILayerZeroEndpointV2, MessagingContext
                 }
             } else if (optionType == ExecutorOptions.OPTION_TYPE_ORDERED_EXECUTION) {
                 // ordered = true;
-            } else {
+            }
+            else {
                 revert IExecutorFeeLib.Executor_UnsupportedOptionType(optionType);
             }
         }
