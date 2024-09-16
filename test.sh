2a5d7b8dfb29f31e6cf32cfeeecc3d65c0e7d1ce
bwhour <juvemeen.xio@gmail.com>
2024-09-16 20:50:47 +0800

    optimize the test script

--- test.sh
+++ test.sh
@@ -5,6 +5,11 @@ CONTRACT_NAME="ExocoreBtcGateway"
 OUTPUT_PATH="/Users/will/go/src/github.com/ExocoreNetwork/bridge_private"
 PRIVATE_KEY="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
 CLIENT_CHAIN_ID=111
+RPC_URL="http://127.0.0.1:8546"
+BTC_ADDRESS="tb1pdwf5ar0kxr2sdhxw28wqhjwzynzlkdrqlgx8ju3sr02hkldqmlfspm0mmh"
+BTC_ADDRESS_BYTES=$(cast --from-utf8 "$BTC_ADDRESS")
+TOKEN_ADDRESS_BYTES="0x2260fac5e5542a773aa44fbcfedf7c193bc2c599000000000000000000000000"
+BRIDGE_ADDRESS="0x0000000000000000000000000000000000000804"
 
 # Step 2: Update ABI
 forge inspect src/core/$CONTRACT_NAME.sol:$CONTRACT_NAME abi > $OUTPUT_PATH/src/abi/exocorebtcgateway.json
@@ -13,9 +18,7 @@ forge inspect src/core/$CONTRACT_NAME.sol:$CONTRACT_NAME abi > $OUTPUT_PATH/src/
 forge flatten src/core/ExocoreBtcGateway.sol | grep -v "Compiling" | grep -v "Solc" | grep -v "Compiler run" > src/core/ExocoreBtcGatewayFlatten.sol
 
 # Step 4: Deploy the contract and capture the output
-output=$(forge create --rpc-url http://127.0.0.1:8546 \
-    --private-key "$PRIVATE_KEY" \
-    src/core/ExocoreBtcGatewayFlatten.sol:ExocoreBtcGateway)
+output=$(forge create --rpc-url $RPC_URL --private-key "$PRIVATE_KEY" src/core/ExocoreBtcGatewayFlatten.sol:ExocoreBtcGateway)
 
 # Step 5: Extract the contract address from the output
 contract_address=$(echo "$output" | grep "Deployed to:" | awk '{print $3}')
@@ -27,34 +30,29 @@ sed -i "" "s/^CONTRACT_ADDRESS=.*/CONTRACT_ADDRESS=\"$contract_address\"/" $OUTP
 echo "$output"
 
 # Step 8: Send a transaction to register an address
-cast send --rpc-url http://127.0.0.1:8546 \
-    --private-key "$PRIVATE_KEY" \
-    $contract_address \
-    "registerAddress(bytes,bytes)" \
-    0x74623170647766356172306b787232736468787732387771686a777a796e7a6c6b6472716c6778386a753373723032686b6c64716d6c6673706d306d6d68 \
-    0x70997970c51812dc3a010c7d01b50e0d17dc79c8000000000000000000000000
+echo "Registering address..."
+register_output=$(cast send --rpc-url $RPC_URL --private-key "$PRIVATE_KEY" "$contract_address" "registerAddress(bytes,bytes)" $BTC_ADDRESS_BYTES 0x70997970c51812dc3a010c7d01b50e0d17dc79c8000000000000000000000000)
+echo "Register address output:"
+echo "$register_output"
+echo
 
 # Step 9: Call the btcToExocoreAddress function
-cast call --rpc-url http://127.0.0.1:8546 \
-    $contract_address \
-    "btcToExocoreAddress(bytes)(bytes)" \
-    0x74623170647766356172306b787232736468787732387771686a777a796e7a6c6b6472716c6778386a753373723032686b6c64716d6c6673706d306d6d68
+echo "Calling btcToExocoreAddress function..."
+btc_to_exocore_output=$(cast call --rpc-url $RPC_URL "$contract_address" "btcToExocoreAddress(bytes)(bytes)" $BTC_ADDRESS_BYTES)
+echo "btcToExocoreAddress output:"
+echo "$btc_to_exocore_output"
+echo
 
 # Step 10: Send a transaction to deposit to an address
-cast send --rpc-url http://127.0.0.1:8546 \
-    --private-key "$PRIVATE_KEY" \
-    --gas-limit 1000000 \
-    0x0000000000000000000000000000000000000804 \
-    "depositTo(uint32,bytes,bytes,uint256)" \
-    ${CLIENT_CHAIN_ID} \
-    0x2260fac5e5542a773aa44fbcfedf7c193bc2c599000000000000000000000000 \
-    0x74623170647766356172306b787232736468787732387771686a777a796e7a6c6b6472716c6778386a753373723032686b6c64716d6c6673706d306d6d68 \
-    2222
+echo "Depositing to address..."
+deposit_output=$(cast send --rpc-url $RPC_URL --private-key "$PRIVATE_KEY" --gas-limit 1000000 $BRIDGE_ADDRESS "depositTo(uint32,bytes,bytes,uint256)" ${CLIENT_CHAIN_ID} $TOKEN_ADDRESS_BYTES $BTC_ADDRESS_BYTES 2222)
+echo "Deposit output:"
+echo "$deposit_output"
+echo
 
 # Step 11: Call the getPrincipalBalance function
-cast call --rpc-url http://127.0.0.1:8546 \
-    0x0000000000000000000000000000000000000804 \
-    "getPrincipalBalance(uint32,bytes,bytes)(uint256)" \
-    ${CLIENT_CHAIN_ID} \
-    0x2260fac5e5542a773aa44fbcfedf7c193bc2c599000000000000000000000000 \
-    0x74623170647766356172306b787232736468787732387771686a777a796e7a6c6b6472716c6778386a753373723032686b6c64716d6c6673706d306d6d68
+echo "Calling getPrincipalBalance function..."
+balance_output=$(cast call --rpc-url $RPC_URL $BRIDGE_ADDRESS "getPrincipalBalance(uint32,bytes,bytes)(uint256)" ${CLIENT_CHAIN_ID} $TOKEN_ADDRESS_BYTES $BTC_ADDRESS_BYTES)
+echo "Principal balance:"
+echo "$balance_output"
+echo
