5ce90bec4a4c269406aa10b32c05a70168e41e37
cloud8little <413074538@qq.com>
2024-10-10 07:27:23 +0000

    update

--- script/3_Setup.s.sol
+++ script/3_Setup.s.sol
@@ -118,7 +118,7 @@ contract SetupScript is BaseScript {
         vm.selectFork(exocore);
         vm.startBroadcast(exocoreValidatorSet.privateKey);
         uint256 nativeFee;
-        for (uint256 i = 1; i < whitelistTokensBytes32.length; i++) {
+        for (uint256 i = 0; i < whitelistTokensBytes32.length; i++) {
             nativeFee = exocoreGateway.quote(
                 clientChainId,
                 abi.encodePacked(Action.REQUEST_ADD_WHITELIST_TOKEN, abi.encodePacked(whitelistTokensBytes32[i],tvlLimits[i]))
