4e5780c22614e88693ccff0926ae2b83d8d500d6
adu <foriteration@gmail.com>
2025-01-21 15:04:13 +0800

    docs: add comment

--- src/core/ClientChainGateway.sol
+++ src/core/ClientChainGateway.sol
@@ -144,6 +144,10 @@ contract ClientChainGateway is
         return (SENDER_VERSION, RECEIVER_VERSION);
     }
 
+    /**
+     * @notice Deploy the reward vault contract.
+     * @dev The reward vault would be deployed with beacon proxy pattern like other vaults
+     */
     // The bytecode returned by the BEACON_PROXY_BYTECODE contract is static, so there is no risk of collision.
     // slither-disable-next-line encode-packed-collision
     function deployRewardVault() external onlyOwner whenNotPaused {
